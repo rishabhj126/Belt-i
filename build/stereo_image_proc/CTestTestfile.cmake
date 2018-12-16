2b694656f75e0408cf7ef649f321cda96ac188c2
vikasnew <vikaskamboj1085>
2018-12-16 14:10:32 +0530

    hackference_india

---
+++ build/stereo_image_proc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vikasnew/hackference_ws/src/stereo_image_proc
+# Build directory: /home/vikasnew/hackference_ws/build/stereo_image_proc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/stereo_vision-master/CMakeFiles/find_target_in_images.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find_target_in_images.dir/src/find_target_in_images.cpp.o"
+  "/home/vikasnew/hackference_ws/devel/lib/stereo_vision/find_target_in_images.pdb"
+  "/home/vikasnew/hackference_ws/devel/lib/stereo_vision/find_target_in_images"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find_target_in_images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
